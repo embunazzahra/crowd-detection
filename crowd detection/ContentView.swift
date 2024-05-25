9d5d4a5d3ea5904624e3f690e1514aa914d417f5
embunazzahra <dhauembunazzahra@gmail.com>
2024-05-25 08:13:02 +0700

    Initial Commit

---
+++ crowd detection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crowd detection
+//
+//  Created by Dhau Embun Azzahra on 25/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crowd detection/crowd_detectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  crowd_detectionApp.swift
+//  crowd detection
+//
+//  Created by Dhau Embun Azzahra on 25/05/24.
+//
+
+import SwiftUI
+
+@main
+struct crowd_detectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
